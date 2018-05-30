07d26892e1b2f0059491f1efcdedc64aae6c6504
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2018-05-30 17:42:03 +0800

    最新

---
+++ Assets/Scripts/ARCameraFocus.cs
@@ -0,0 +1,37 @@
+﻿//#if VUFORIA_ANDROID_SETTINGS
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+
+public class ARCameraFocus: MonoBehaviour
+{
+
+    void Start()
+    {
+
+        CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+
+    }
+
+    
+
+  
+   public void AutoFocus()
+    {
+        StartCoroutine(TriggerAutoFocusAndEnableContinuousFocusIfSet());
+    }
+
+    private IEnumerator TriggerAutoFocusAndEnableContinuousFocusIfSet()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+        yield return new WaitForSeconds(1.0f);
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+
+    }
+}
+
+//#endif
